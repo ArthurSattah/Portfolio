c8cdb6110cce3ec575528f7670d2afff25d0ad5a
unknown <arthursattah@gmail.com>
2025-01-26 21:45:29 +0300

    edit one project

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -12,42 +12,46 @@ const Project = () => {
                 <div className="leftPro">
                     {projects.map((project, id) => {
                         return (
-                            <img key={id} className={`${id === number ? "selected" : ""} leftProImg`} 
-                            src={require(`../../assets/${project.imageSrc}`)} alt={project.title} onClick={() => { setNumber(id) }} />
+                            <img key={id} className={`${id === number ? "selected" : ""} leftProImg`}
+                                src={require(`../../assets/${project.imageSrc}`)} alt={project.title} onClick={() => { setNumber(id) }} />
                         );
                     })}
                 </div>
                 <div className="rightPro">
                     {projects.map((project, id) => {
-                            return (
-                                <div className= {id===number ? "card" : "notSelected"} key="id">
-                                    <img src={require(`../../assets/${project.imageSrc}`)} alt={project.title} className='cardImg' />
-                                    <div className="cardTalk">
-                                        <div className="cardTitle">{project.title}</div>
-                                        <div className="cardDesc">{project.description}</div>
-                                        <div className='cardSkills'>
-                                            {project.skills.map((skill, id) => {
-                                                return (
-                                                    <div className="cardSkill" key={id}>
-                                                        {skill}
-                                                    </div>
-                                                );
-                                            })}
-                                        </div>
-                                        <div className='cardLinks'>
-                                            <a href={project.demo} className="cardItem" target="_blank">
-                                                Try It
-                                            </a>
-                                            <a href={project.source} className="cardItem" target="_blank">
-                                                Source Code
-                                            </a>
+                        return (
+                            <div className={id === number ? "card" : "notSelected"} key="id">
+                                <img src={require(`../../assets/${project.imageSrc}`)} alt={project.title} className='cardImg' />
+                                <div className="cardTalk">
+                                    <div className="cardTitle">{project.title}</div>
+                                    <div className="cardDesc">{project.description}</div>
+                                    <div className='cardSkills'>
+                                        {project.skills.map((skill, id) => {
+                                            return (
+                                                <div className="cardSkill" key={id}>
+                                                    {skill}
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                    <div className='cardLinks'>
+                                        {
+                                            project.title === "Travel Advisor" ? <></> :
+                                                <a href={project.demo} className="cardItem" target="_blank">
+                                                    Try It
+                                                </a>
+                                        }
 
-                                        </div>
-                                        
+                                        <a href={project.source} className="cardItem" target="_blank">
+                                            Source Code
+                                        </a>
+
                                     </div>
 
                                 </div>
-                            );
+
+                            </div>
+                        );
                     })}
                 </div>
             </div>
@@ -56,4 +60,3 @@ const Project = () => {
 }
 
 export default Project
- 
