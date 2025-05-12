249f5cf4ca376cad344a762f56ca28a82b73b811
Arthur Sattah <arthursattah@gmail.com>
2025-05-12 11:19:53 +0300

    edit the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,43 +1,48 @@
 import React, { useState } from 'react';
 import './navbar.css';
+import AnimationFromUp from '../framerMotion/animationFromUp/AnimationFromUp';
+
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <div className="container">
-            <a href='https://arthursattah.github.io/Portfolio/' className='title'>
-                Portfolio
-            </a>
-            <div className='menus'>
-                <img src={menuOpen === false ? require("../../assets/nav/menuIcon.png") : require("../../assets/nav/closeIcon.png")}
-                    alt="menu-Icon"
-                    className='imgIcon' onClick={() => setMenuOpen(!menuOpen)} />
+        <AnimationFromUp>
 
-                <ul className={`items ${menuOpen ? "" : "close"} `}>
-                    <li className="item">
-                        <a href="#about" onClick={() => setMenuOpen(false)}>
-                            About
-                        </a>
-                    </li>
-                    <li className="item" onClick={() => setMenuOpen(false)}>
-                        <a href="#experiance">
-                            Experiance
-                        </a>
-                    </li>
-                    <li className="item" onClick={() => setMenuOpen(false)}>
-                        <a href="#projects">
-                            Projects
-                        </a>
-                    </li>
-                    <li className="item" onClick={() => setMenuOpen(false)}>
-                        <a href="#contact">
-                            Contact
-                        </a>
-                    </li>
-                </ul>
+            <div className="container">
+                <a href='https://arthursattah.github.io/Portfolio/' className='title'>
+                    Portfolio
+                </a>
+                <div className='menus'>
+                    <img src={menuOpen === false ? require("../../assets/nav/menuIcon.png") : require("../../assets/nav/closeIcon.png")}
+                        alt="menu-Icon"
+                        className='imgIcon' onClick={() => setMenuOpen(!menuOpen)} />
+
+                    <ul className={`items ${menuOpen ? "" : "close"} `}>
+                        <li className="item">
+                            <a href="#about" onClick={() => setMenuOpen(false)}>
+                                About
+                            </a>
+                        </li>
+                        <li className="item" onClick={() => setMenuOpen(false)}>
+                            <a href="#experiance">
+                                Experiance
+                            </a>
+                        </li>
+                        <li className="item" onClick={() => setMenuOpen(false)}>
+                            <a href="#projects">
+                                Projects
+                            </a>
+                        </li>
+                        <li className="item" onClick={() => setMenuOpen(false)}>
+                            <a href="#contact">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
-
-        </div>
+        </AnimationFromUp>
     )
 }
 
